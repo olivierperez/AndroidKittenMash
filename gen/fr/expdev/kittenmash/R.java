2302fd7c24d89c98ba7a61083ebcb5dd9d2582a3
Olivier PEREZ <olivier.perez@easyteam.fr>
2012-11-23 23:41:36 +0100

    Ajout d'un style sur le bouton

--- gen/fr/expdev/kittenmash/R.java
+++ gen/fr/expdev/kittenmash/R.java
@@ -10,6 +10,17 @@ package fr.expdev.kittenmash;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int btn_focus_1=0x7f060006;
+        public static final int btn_focus_2=0x7f060007;
+        public static final int btn_focus_border=0x7f060008;
+        public static final int btn_normal_1=0x7f060000;
+        public static final int btn_normal_2=0x7f060001;
+        public static final int btn_normal_border=0x7f060002;
+        public static final int btn_press_1=0x7f060003;
+        public static final int btn_press_2=0x7f060004;
+        public static final int btn_press_border=0x7f060005;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int kitten_01=0x7f020001;
@@ -22,21 +33,22 @@ public final class R {
         public static final int kitten_08=0x7f020008;
         public static final int kitten_09=0x7f020009;
         public static final int kitten_10=0x7f02000a;
+        public static final int km_button=0x7f02000b;
     }
     public static final class id {
-        public static final int best_kitten=0x7f070001;
-        public static final int fight_button=0x7f070000;
-        public static final int kitten1=0x7f070002;
-        public static final int kitten2=0x7f070003;
-        public static final int menu_settings=0x7f070004;
+        public static final int best_kitten=0x7f080001;
+        public static final int fight_button=0x7f080000;
+        public static final int kitten1=0x7f080002;
+        public static final int kitten2=0x7f080003;
+        public static final int menu_settings=0x7f080004;
     }
     public static final class layout {
         public static final int activity_best_kitten=0x7f030000;
         public static final int activity_fight=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_best_kitten=0x7f060000;
-        public static final int activity_fight=0x7f060001;
+        public static final int activity_best_kitten=0x7f070000;
+        public static final int activity_fight=0x7f070001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
